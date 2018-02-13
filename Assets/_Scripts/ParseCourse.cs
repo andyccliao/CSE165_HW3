1e7892d486f569bce2201477ae93007340772f04
acliao <acliao@ucsd.edu>
2018-02-12 19:11:01 -0800

    Scaled map and started Course loading script

---
+++ Assets/_Scripts/ParseCourse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class ParseCourse : MonoBehaviour {
+	// !!! REMEMBER THAT ALL RACECOURSE FILES ARE IN INCHES, AND MUST BE CONVERTED TO METERS
+	List<Vector3> checkpointPos;
+
+	// Add Checkpoints to map
+	void Start () {
+		string path = EditorUtility.OpenFilePanel("Choose a level", Directory.GetCurrentDirectory() + @"\Courses", "png");
+		if (path == null)
+			return;
+		string[] coordinates = File.ReadAllLines (path);
+		checkpointPos = new List<Vector3> (coordinates.Length);
+
+		/* Read coordinates */
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
